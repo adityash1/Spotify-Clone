4209528f91eb589f4986fd70e308dc624dba019b
adityash1 <aditya167411@gmail.com>
2022-07-23 11:20:13 +0530

    addded srollable playlist section in sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -36,6 +36,21 @@ const navMenu = [
   },
 ];
 
+const musicMenu = [
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Favorites",
+    icon: MdFavorite,
+    route: "/favorites",
+  },
+];
+
+const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`);
+
 const Sidebar = () => {
   return (
     <Box
@@ -45,7 +60,7 @@ const Sidebar = () => {
       paddingX="5px"
       color="gray"
     >
-      <Box paddingY="20px">
+      <Box paddingY="20px" height="100vh">
         <Box width="120px" marginBottom="20px" paddingX="20px">
           <NextImage src="/logo.svg" height={60} width={120} />
         </Box>
@@ -68,6 +83,54 @@ const Sidebar = () => {
               </ListItem>
             ))}
           </List>
+        </Box>
+        <Box marginTop="20px" marginBottom="20px">
+          <List spacing={2}>
+            {musicMenu.map((item) => (
+              <ListItem key={item.name} paddingX="20px" fontSize="16px">
+                <LinkBox>
+                  <NextLink href={item.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={item.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {item.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Divider color="gray.800" />
+        <Box
+          height="66%"
+          overflowY="auto"
+          paddingY="20px"
+          sx={{
+            "&::-webkit-scrollbar": {
+              width: "16px",
+              borderRadius: "8px",
+              backgroundColor: `rgba(0, 0, 0, 0.2)`,
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: `rgba(0, 0, 0, 0.5)`,
+            },
+          }}
+        >
+          {playlists.map((playlist) => (
+            <List spacing={2}>
+              <ListItem key={playlist} paddingX="20px">
+                <LinkBox>
+                  <NextLink href="/" passHref>
+                    <LinkOverlay>{playlist}</LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            </List>
+          ))}
         </Box>
       </Box>
     </Box>
