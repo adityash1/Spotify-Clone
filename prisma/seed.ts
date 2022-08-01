bf9f4466ad9321c1acd9dbce1009ebb120d501d9
adityash1 <aditya167411@gmail.com>
2022-08-01 16:50:35 +0530

    adding relations & seed db changes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -33,6 +33,27 @@ const run = async () => {
       password: bcrypt.hashSync("password", salt),
     },
   });
+
+  const songs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(0).map(async (_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist ${i + 1}`,
+          user: {
+            connect: {
+              id: user.id,
+            },
+          },
+          songs: {
+            connect: songs.map((song) => ({
+              id: song.id,
+            })),
+          },
+        },
+      });
+    })
+  );
 };
 
 run()
