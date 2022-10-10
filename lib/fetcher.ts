7e759d2b432e7510f6091fdd7326532ee282550a
adityash1 <aditya167411@gmail.com>
2022-10-10 23:44:56 +0530

    Update fetcher.ts

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,9 +1,10 @@
-export default function(url: string, data = undefined) {
-    return fetch(`${window.location.origin}/api${url}`, {
-        method: data ? "POST" : "GET",
-        credentials: "include",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: data ? JSON.stringify(data) : undefined,
+export default function (url: string, data = undefined) {
+  return fetch(`${window.location.origin}/api${url}`, {
+    method: data ? "POST" : "GET",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: data ? JSON.stringify(data) : undefined,
+  });
 }
